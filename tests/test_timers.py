a957fdd46e674ca98445e2bf18bba80d93ae45cd
akliang <none@none.com>
2022-03-14 05:15:49 +0000

    initial timer test

---
+++ tests/test_timers.py
@@ -0,0 +1,20 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import pytest
+
+import cooklang_processor as c
+
+@pytest.fixture
+def recipe(tmp_path):
+  d = tmp_path / "recipe.cook"
+  yield d
+
+def test_anon_timer(recipe):
+  recipe.write_text("hello ~{5%minutes}")
+  i = c.process(recipe)
+  assert i['cookware'][0] == ("", "5 minutes")
+
+def test_named_timer(recipe):
+  recipe.write_text("hello ~egg{5%minutes}")
+  i = c.process(recipe)
+  assert i['cookware'][0] == ("egg", "5 minutes")
